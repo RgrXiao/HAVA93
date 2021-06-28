0594dc1575db8b66f813f7b3ce9928bb6d049956
RgrXiao <yuanyal1311@163.com>
2021-06-28 21:48:10 +0800

    Create stockY.php

---
+++ admin/stockY.php
@@ -0,0 +1,44 @@
+<?php
+
+
+$db = mysqli_connect('localhost','hv18624_shirts','Reading@10','hv18624_shirts');
+$errmsg = $db->connect_error ;
+if ($errmsg != null) echo "Error: $errmsg" ;
+else
+  {
+
+
+$query="SELECT * FROM Products WHERE Stock = 'N' " ;
+ $res = $db->query($query);
+
+ if ($res)
+{
+
+require_once("../header.html");  
+         
+      echo '      <div class="container"> ';
+      echo '<form action="stockModY.php" method="post"> ';
+      echo '<label>Product:';
+    echo '<select name="product">';
+      echo '<option>Select One</option>';
+
+while($row = mysqli_fetch_assoc($res))
+{
+
+        $name = $row['productName'];
+        echo '<option  name="product"  value="' .$name. '">' .$name. '</option>';
+  
+}
+
+     echo '</select>';
+     echo '</label>';
+     echo '	<button name="stockModY" type="submit">Click to add selected item back in stock.</button> ';
+     echo  ' </form> ';
+}
+
+$db->close();
+
+}
+
+
+?>
