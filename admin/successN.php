eb5bed22f738e801f7b75873aff5db201a084499
RgrXiao <yuanyal1311@163.com>
2021-07-02 07:48:30 +0800

    Create successN.php

---
+++ admin/successN.php
@@ -0,0 +1,10 @@
+<?php require_once("headerAdmin.html"); ?>
+
+<h1> Success! Product is currently out of stock! </h1>
+
+<a href="stockY.php">Add Products back in stock.</a>
+<br> <br>
+<a href="stockN.php">Remove Products from stock.</a>
+
+
+<?php require_once("../footer.html"); ?>
